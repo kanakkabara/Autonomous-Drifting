9c440f0c3aae56c2c8d9add0ac1be36e86d7fca5
Kanak Kabara <kanakkabara@hotmail.com>
2018-02-26 18:48:36 +0800

    Python Protobuf files

--- fyp_ws/src/drift_car/scripts/rl/xbeeTries/xbeeTest.py
+++ fyp_ws/src/drift_car/scripts/rl/xbeeTries/xbeeTest.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 from xbee.thread import XBee
+import messages_pb2
 import serial
 import time
 
@@ -14,11 +16,22 @@ xbee = XBee(ser)
 # Continuously read and print packets
 while True:
         try:
-           response = xbee.wait_read_frame()
-           print (response)
-           while True:
-               xbee.tx(dest_addr=b'\x00\x00', data='Received by remote')
-               time.sleep(1)
+            response = xbee.wait_read_frame()
+            action = messages_pb2.Action()
+            action.ParseFromString(response['rf_data'].decode("utf-8"))
+            print(action.throttle)
+            print(action.steering)
+
+            # state = messages_pb2.State()
+            # print(state.ParseFromString(response['rf_data']))
+
+        #   while True:
+            # action = messages_pb2.Action()
+            # action.throttle = 40
+            # action.steering = 70
+            # xbee.tx(dest_addr=b'\x00\x00', data=action.SerializeToString())
+
+            # time.sleep(1)
         except KeyboardInterrupt:
-           break
-           ser.close()
+            break
+            ser.close()
