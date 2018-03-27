9e093241431903f16309816ac2592f2301fe2803
Kanak Kabara <kanakkabara@hotmail.com>
2018-03-28 01:11:34 +0800

    Updated Joystick Gazebo script

--- fyp_ws/src/drift_car_gazebo_control/scripts/joystick_gazebo.py
+++ fyp_ws/src/drift_car_gazebo_control/scripts/joystick_gazebo.py
@@ -5,14 +5,13 @@ import gym_drift_car
 from sensor_msgs.msg import Joy
 import subprocess
 
-global throtle, servo, drifting, env
-throtle = 250
-driftThrotle = 1720
-THROTLE_STEP = 2
-servo = 0
-drifting = False
-
+MIN_THROTTLE = 1650
+MAX_THROTTLE = 1770
+MAX_SERVO = 0.785398
 env = None
+
+global drifting
+drifting = False
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     leftSpan = leftMax - leftMin
@@ -25,23 +24,24 @@ def callback(data):
         env.reset()
         return
         
-    global throtle, servo, drifting, driftThrotle
-    servo = data.axes[3] * 0.46
-    throtle = translate(data.axes[2], -1.0, 1.0, driftThrotle, 1650)
+    servo = data.axes[3] * MAX_SERVO
+    throtle = translate(data.axes[2], -1.0, 1.0, MAX_THROTTLE, MIN_THROTTLE)
 
+    global drifting
     if data.buttons[4] == 1: # LB Button to stop drift throttle
         drifting = False
     if data.buttons[5] == 1: # RB Button to start drift throttle
         drifting = True
     
     if drifting:
-        env.step((driftThrotle, servo))
+        # print((MAX_THROTTLE, servo))
+        state, _, _, _ = env.step((MAX_THROTTLE, servo))
+        print(state[-4:])
     else:
         env.step((throtle, servo))
 
 if __name__=="__main__":
-    global env
-    env = gym.make('DriftCarGazeboContinuous-v0')
+    env = gym.make('DriftCarGazeboContinuous4WD-v0')
     env.reset()
     env.render()
 
@@ -52,8 +52,8 @@ if __name__=="__main__":
     print("Joystick Controller for Gazebo Drift Car")
     print("========================================\n")
     
-    print("X for acceleration")
-    print("Y to toggle constant speed")
+    print("LT for acceleration")
+    print("RB/LB to toggle constant speed")
     print("B for hard reset")
 
     rospy.spin()
